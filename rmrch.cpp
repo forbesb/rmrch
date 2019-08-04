51baff3b46f0db364df06557208c942e08281cef
Brian Forbes <baforbes@uwaterloo.ca>
2019-08-04 10:38:08 -0400

    Shorten some functions, break phong a bit less

--- rmrch.cpp
+++ rmrch.cpp
@@ -4,25 +4,29 @@
 #include <stdio.h>
 #include <math.h>
 
+#define U return
+
 struct vector
 {
     float x, y, z;
     vector(){}
     vector(float a, float b, float c){x = a; y = b; z = c;}
-    vector operator+(vector r){return vector(x+r.x, y+r.y, z+r.z);} // vector add
-    float operator*(vector r){return x * r.x + y * r.y + z * r.z;} // vector dot
-    vector operator*(float r){return vector(x*r, y*r, z*r);} //scalar multiply
-    vector operator^(vector r){return vector(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.z);} // vector cross
-    vector normalize(){return (*this)*(1 / sqrt((*this)*(*this)));}
+    vector operator+(vector r){U vector(x+r.x, y+r.y, z+r.z);} // vector add
+    float operator*(vector r){U x * r.x + y * r.y + z * r.z;} // vector dot
+    vector operator*(float r){U vector(x*r, y*r, z*r);} //scalar multiply
+    vector operator^(vector r){U vector(y * r.z - z * r.y, z * r.x - x * r.z, x * r.y - y * r.z);} // vector cross
+    vector operator!(){U this->normalize();}
+    vector normalize(){U (*this)*(1 / sqrt((*this)*(*this)));}
 };
 
-float signed_distance(vector p)
+float S(vector p)
 {
     return sqrt(p*p)-1;
 }
 
 #define far 10.0f
 #define epsilon 0.001f
+#define E epsilon
 
 
 typedef float f;
@@ -30,27 +34,23 @@ typedef int i;
 typedef vector v;
 
 float march_ray(v o,v d){
-    f t=0;
-    for(i s=100;s--;){f m = signed_distance(o+d*t);t+=m;if(m<epsilon)break;if(m>far)break;}
-    return t;
+    f t=0;for(i s=100;s--;){f m = S(o+d*t);t+=m;if(m<E)break;if(m>far)break;}U t;
 }
-
-vector normal(vector p){
-    return vector(signed_distance(v(p.x+epsilon, p.y, p.z)) - signed_distance(v(p.x-epsilon, p.y, p.z)),
-                   signed_distance(v(p.x, p.y+epsilon, p.z)) - signed_distance(v(p.x, p.y - epsilon, p.z)),
-                   signed_distance(v(p.x, p.y, p.z+epsilon)) - signed_distance(v(p.x, p.y, p.z-epsilon))).normalize();}
+v normal(v p){U !v(S(v(p.x+E,p.y,p.z))-S(v(p.x-E,p.y,p.z)),S(v(p.x,p.y+E,p.z))-S(v(p.x,p.y-E,p.z)),S(v(p.x,p.y,p.z+E))-S(v(p.x, p.y, p.z-E)));}
 
 vector phong(v n, v l, v d)
 {
-    static float a = 1.3f; static v ka(0.1, 0.1, 0.1), kd(0.3f, 0.0f, 0.1f), ks(0.5f, 0.8f, 0.9f);
+    static float a = 10.1f; static v ka(0.1, 0.1, 0.1), kd(0.3f, 0.0f, 0.1f), ks(0.3f, 0.0f, 0.1f);
     vector reflection = n*(l*n)*2+(l*-1);
     vector one (1.0f, 1.0f, 1.0f);
 
     vector ambient = ka;
     vector diffuse = kd*(l*n);
     vector refl = ks *(pow(abs(reflection*d), a));
+
     if (DEBUG) printf("%f %f %f | %f\n", reflection.x, reflection.y, reflection.z, reflection*d);
     if (DEBUG) printf("%f %f %f, %f %f %f, %f %f %f\n", ambient.x, ambient.y, ambient.z, diffuse.x, diffuse.y, diffuse.z, refl.x, refl.y, refl.z);
+    //vector color = ambient + diffuse + refl;
     vector color = ambient + diffuse + refl;
     if (DEBUG) printf("%f %f %f\n", color.x, color.y, color.z);
 
