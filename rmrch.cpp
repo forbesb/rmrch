21d73c17de51d26fa2a45029f916dbb48d3fc1aa
Brian Forbes <baforbes@uwaterloo.ca>
2019-08-04 10:16:49 -0400

    Phong shading - broken reflected colour

--- rmrch.cpp
+++ rmrch.cpp
@@ -1,3 +1,5 @@
+#define DEBUG 0
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -20,14 +22,41 @@ float signed_distance(vector p)
 }
 
 #define far 10.0f
-#define epsilon 0.001
+#define epsilon 0.001f
+
+
 typedef float f;
 typedef int i;
+typedef vector v;
 
-float march_ray(vector o, vector d){
+float march_ray(v o,v d){
     f t=0;
     for(i s=100;s--;){f m = signed_distance(o+d*t);t+=m;if(m<epsilon)break;if(m>far)break;}
-    return t;}
+    return t;
+}
+
+vector normal(vector p){
+    return vector(signed_distance(v(p.x+epsilon, p.y, p.z)) - signed_distance(v(p.x-epsilon, p.y, p.z)),
+                   signed_distance(v(p.x, p.y+epsilon, p.z)) - signed_distance(v(p.x, p.y - epsilon, p.z)),
+                   signed_distance(v(p.x, p.y, p.z+epsilon)) - signed_distance(v(p.x, p.y, p.z-epsilon))).normalize();}
+
+vector phong(v n, v l, v d)
+{
+    static float a = 1.3f; static v ka(0.1, 0.1, 0.1), kd(0.3f, 0.0f, 0.1f), ks(0.5f, 0.8f, 0.9f);
+    vector reflection = n*(l*n)*2+(l*-1);
+    vector one (1.0f, 1.0f, 1.0f);
+
+    vector ambient = ka;
+    vector diffuse = kd*(l*n);
+    vector refl = ks *(pow(abs(reflection*d), a));
+    if (DEBUG) printf("%f %f %f | %f\n", reflection.x, reflection.y, reflection.z, reflection*d);
+    if (DEBUG) printf("%f %f %f, %f %f %f, %f %f %f\n", ambient.x, ambient.y, ambient.z, diffuse.x, diffuse.y, diffuse.z, refl.x, refl.y, refl.z);
+    vector color = ambient + diffuse + refl;
+    if (DEBUG) printf("%f %f %f\n", color.x, color.y, color.z);
+
+    return color;
+}
+                
 
 
 int main()
@@ -38,16 +67,19 @@ int main()
     vector forward(0.0f, 0.0f, -1.0f);
     vector up     (0.0f, 1.0f, 0.0f);
     vector horiz  (1.0f, 0.0f,0.0f);
+    vector light  (0.0f, 2.0f, 2.0f);
     
-    for (int y = 512; y--;){
-    for (int x = 512; x--;)
-    {
+    for (int y = 512; y--;)
+    for (int x = 512; x--;){
         float a = (((float)y) - 256.0)*(1.0f/512.0f);
         float b = (((float)x) - 256.0)*(1.0f/512.0f);
-        vector direction = forward + up*a + horiz*b;
-        vector color(0, 0, 0);
-        if (march_ray(origin, direction) < far) color.x = 200;
-        //printf("%f %f %f, %f %f, %f\n", direction.x, direction.y, direction.z, a, b, color.x);
-        printf("%c%c%c",(int)color.x, (int)color.y, (int)color.z); 
-    }}
+        f t;
+        vector direction = (forward + up*a + horiz*b).normalize();
+        vector color(0.9f, 0.9f, 0.9f);
+        if ((t=march_ray(origin, direction)) < far) { 
+            v p = origin+direction*t; 
+            color = phong(normal(p), (light+(p*-1)).normalize(), direction * (-1.0f));
+        }
+        if (!DEBUG) printf("%c%c%c",(int)(color.x*255.0f), (int)(color.y*255.0f), (int)(color.z*255.0f)); 
+    }
 }
